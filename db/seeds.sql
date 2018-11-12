646ee59dca777411fc3c1a109ae6b03a39568c13
Dmitrii Zakharov <dzakharov@splunk.com>
2018-11-12 14:33:46 -0800

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Bigmac', false);
+INSERT INTO burgers (name, devoured) VALUES ('Quarterpounder', false);
+INSERT INTO burgers (name, devoured) VALUES ('Whopper', false);
+
