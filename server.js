5c01f2fd82d9ce72c5f5080c17452fbf96a88a0b
Dmitrii Zakharov <dzakharov@splunk.com>
2018-11-15 09:23:45 -0800

    Fixed server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 
+const host = '0.0.0.0';
 var PORT = process.env.PORT || 8080;
 
 var app = express();
@@ -23,7 +24,7 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, host, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
